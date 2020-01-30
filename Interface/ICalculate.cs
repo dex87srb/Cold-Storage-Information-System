70948a7daf801178a47039cdacf859127a5f7369
Daniel <47319916+dex87srb@users.noreply.github.com>
2020-01-30 10:46:49 +0100

    Add files via upload

---
+++ Interface/ICalculate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Information_System
+{
+   public interface ICalculate
+    {    
+        double Calculate_balance();
+        string WriteFullLine(string value);
+        double Database_Calculate();
+
+
+    }
+}
